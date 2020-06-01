f3423eac541df294a8b2b8ff70730b9f319a6883
tabiojisann <takumiezumi@icloud.com>
2020-06-01 22:45:17 +0900

    UI変更

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -11,7 +11,7 @@
       </div>
 
       @guest
-      <div class="form-inline mt-5" style="margin-left: 40%; m ">
+      <div class="form-inline mt-4" style="margin-left: 40%; m ">
         <div class="md-form my-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
         </div>
@@ -24,51 +24,60 @@
       @endguest
 
       @auth
-     
+
         <div class="userName">
           <p class="user">USER NAME : {{ Auth::user()->name }}</p>
         </div> 
-
-        <div class="form-inline mt-5" style="margin-left: 40%;">
+        
+      <div class="d-flex justify-content-around mt-5">
+        <div class="mt-5 col-md-2">
           <div class="md-form my-0">
               <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
           </div>
-          <a href="{{ route('login') }}">
-            <button type="button" class="btn btn-mdb-color searchButton">Search</button>
-          </a>
+          <button class="btn btn-unique mt-5" style="border-radius: 30px;"><i class="far fas fa-search"> Search</i></button>
         </div> 
+  
         <form method="POST" action="{{ route('articles.store') }}">
           @csrf
-        <div class="md-form">
-          <label>タイトル</label>
-          <input type="text" name="title" class="form-control" required>
-        </div>
-        <div class="form-group">
-          <label>URL</label>
-          <input name="url" required class="form-control">
-        </div>
-        <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
-        </form>
-          
-      @foreach($articles as $article)
-      <div class="card">
-        <p>{{ $article->title }}</p>
-        <p>{{ $article->url }}</p>
-        <form action="{{ action('ArticleController@destroy', $article->id) }}" id="form_{{ $article->id }}" method="post" style="display:inline">
-          @csrf
-          @method('DELETE')
-          <button type="submit" class="btn btn-danger">削除</button>
-        </form>
+          <div class="d-flex justify-content-center">
+            <div class="col-md-7 mt-4">
+              <div class="md-form">
+                <label>Title</label>
+                <input type="text" name="title" class="form-control" required>
+              </div>
+            </div>
+            <div class="col-md-7 mt-4">  
+              <div class="md-form">
+                <label>URL</label>
+                <input type="text" name="url" required class="form-control">
+              </div>
+            </div>  
+          </div>  
+          <div class="col-md-5 pt-4 ml-5">
+            <button type="submit" class="btn btn-default btn-block mt-1 ">SEND</button>
+          </div>
+        </form> 
       </div>
-
-   
-      @endforeach
-      {{ $articles->links() }}
-      </div>
-      @endauth
 
      
 
+      <!-- 記事一覧 -->
+        @foreach($articles as $article)
+        <div class="card">
+          <p>{{ $article->title }}</p>
+          <p>{{ $article->url }}</p>
+          <form action="{{ action('ArticleController@destroy', $article->id) }}" id="form_{{ $article->id }}" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">削除</button>
+          </form>
+        </div>
+        @endforeach
+      <!-- 記事一覧 -->
 
-  </div>
+        {{ $articles->links() }}
+
+      @endauth
+    </div>
+      
 @endsection
