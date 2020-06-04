3eedef668ceae7a96a8e09bc3c3912b28e3ebada
tabiojisann <takumiezumi@icloud.com>
2020-06-04 22:42:56 +0900

    UI修正

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,33 +6,35 @@
 @section('content')
   @extends('nav')
     <div class="wrapper  grey lighten-4" style="height: 200vh;">
-      <div class="title">
+      <div class="title mt-5">
         <h1 class="text-center">Save MyArticle</h1>
-      </div>
-      
+      </div>   
+
       @guest
         @include('modal')
       @endguest
 
       @auth
-        <div class="userName">
-          <p class="user">USER NAME : {{ Auth::user()->name }}</p>
+        <div style="width: 100%;">
+          <p class="text-center">USER NAME : {{ Auth::user()->name }}</p>
         </div> 
+
+        @include('error_card_list')
         
         <div class="d-flex justify-content-around mt-5">
           <form method="get">
             @foreach ($request as $key=>$value)
               @if ($key!="keyword")
-                <input type="hidden" name="{{$key}}" value="{{$value}}" />
+                <input type="hidden" name="{{$key}}" value="{{$value}}"/>
               @endif
-            @endforeach
+            @endforeach 
             <div class="md-form my-0 mt-5">
-                <input name="keyword" class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" value="{{$request['keyword'] ?? ''}}">
+                <input name="keyword" class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" value="{{$request['keyword'] ?? ''}}" required>
             </div>
             <button class="btn btn-unique mt-5" style="border-radius: 30px;"><i class="far fas fa-search"> Search</i></button>
           </form>
     
-          @include('error_card_list')
+
 
           <form method="POST" action="{{ route('articles.store') }}">
             @csrf
@@ -55,35 +57,23 @@
             </div>
           </form> 
         </div>
-
-        <!-- <form method="get">
-          @foreach ($request as $key=>$value)
-            @if ($key!="keyword")
-              <input type="hidden" name="{{$key}}" value="{{$value}}" />
-            @endif
-          @endforeach
-          <input type="text" class="form-control" name="keyword" placeholder="Name" value="{{$request['keyword'] ?? ''}}">
-          <button class="btn">Search</button>
-        </form> -->
+   
 
         <!-- 記事一覧 -->
        
         @foreach($articles as $article)
           @if( Auth::id() === $article->user_id )
-            <div class="card mx-auto" style="width: 60%; margin-top: 100px;">
-              <!--Card content-->
+            <div class="card mx-auto" style="width: 60%; margin-top: 50px;">
               <div class="card-body">
-                <!--Title-->
-                <div style="border-bottom: solid 1px lightgrey;">
-                  <h4 class="card-title">{{ $article->title }}</span></h4>
-                </div>
-                <!--Text-->
-                <p class="mt-4">{{ $article->url }}</p>
                 <form action="{{ action('ArticleController@destroy', $article->id) }}" id="form_{{ $article->id }}" method="post" style="display:inline">
                   @csrf
                   @method('DELETE')
-                  <button type="submit" style="color: red;"><i class="fas fa-times "></i></button>
+                  <button type="submit" style="color: red; float: right;"><i class="fas fa-times "></i></button>
                 </form>
+                <div style="border-bottom: solid 1px lightgrey;">
+                  <h4 class="card-title">{{ $article->title }}</span></h4>
+                </div>
+                <p class="mt-4">{{ $article->url }}</p>
               </div>
             </div>
           @endif
